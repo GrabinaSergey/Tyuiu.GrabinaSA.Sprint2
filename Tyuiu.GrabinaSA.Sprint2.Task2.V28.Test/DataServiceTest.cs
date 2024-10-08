d764c3c67ed03699adc22ab9b83eb57a58a53cde
GrabinaSergey <dieumortx@gmail.com>
2024-10-08 21:10:05 +0500

    Task2

--- Tyuiu.GrabinaSA.Sprint2.Task2.V28.Test/DataServiceTest.cs
+++ Tyuiu.GrabinaSA.Sprint2.Task2.V28.Test/DataServiceTest.cs
@@ -9,7 +9,7 @@ namespace Tyuiu.GrabinaSA.Sprint2.Task2.V28.Test
         {
             DataService ds = new DataService();
             int x = 2;
-            int y = 4;
+            int y = 5;
             bool res = ds.CheckDotInShadedArea(x, y);
             bool wait = true;
             Assert.AreEqual(wait, res);
