edf7fda6b2b986b9221c445289fc39349abfddac
GrabinaSergey <dieumortx@gmail.com>
2024-10-08 19:52:41 +0500

    Task1

---
+++ Tyuiu.GrabinaSA.Sprint2.Task1.V3.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.GrabinaSA.Sprint2.Task1.V3.Lib
+{
+    public class DataService : ISprint2Task1V3
+    {
+        public bool[] GetLogicOperations(int a, int b, int c, int d)
+        {
+            bool[] res = new bool[6];
+            // a = 185, b = 316, c = 134, d = 134.
+            res[0] = (a < b) | (c > d); // true
+            res[1] = (a < b) & (c > d); // false
+            res[2] = (a > b) || (c > d); // false
+            res[3] = (a > b) && (c > d); // false
+            res[4] = !(res[0]); // false
+            res[5] = (a > b) ^ (c < d); //false
+            return res;
+
+        }
+    }
+}
---
+++ Tyuiu.GrabinaSA.Sprint2.Task1.V3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using Tyuiu.GrabinaSA.Sprint2.Task1.V3.Lib;
+namespace Tyuiu.GrabinaSA.Sprint2.Task1.V3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            int a = 185;
+            int b = 316;
+            int c = 134;
+            int d = 134;
+            bool[] res = new bool[6];
+            res = ds.GetLogicOperations(a, b, c, d);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("A = " + a);
+            Console.WriteLine("B = " + b);
+            Console.WriteLine("C = " + c);
+            Console.WriteLine("D = " + d);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(res[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
