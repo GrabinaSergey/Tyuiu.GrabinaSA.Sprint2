30442a12c62e6fb75f14daa9a12a4280646c4213
GrabinaSergey <dieumortx@gmail.com>
2024-10-15 16:07:43 +0500

    Task7

---
+++ Tyuiu.GrabinaSA.Sprint2.Task7.V1.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.GrabinaSA.Sprint2.Task7.V1.Lib;
+namespace Tyuiu.GrabinaSA.Sprint2.Task7.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            double x = 0.1, y = 0.5;
+            bool wait = true;
+            var res = ds.CheckDotInShadedArea(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.GrabinaSA.Sprint2.Task7.V1/Program.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.GrabinaSA.Sprint2.Task7.V1.Lib;
+namespace Tyuiu.GrabinaSA.Sprint2.Task7.V1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y;
+            Console.WriteLine("Введите значение переменной X:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной Y:");
+            y = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            var res = ds.CheckDotInShadedArea(x, y);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
